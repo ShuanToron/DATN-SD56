80a98f2cf4929af57e14c4f4347e0b5d66fe84cd
DESKTOP-6LU17M8\nguye <anndph23528@fpt.edu.vn>
2023-10-13 01:54:38 +0700

    api cart

--- src/main/java/com/example/datnsd56/entity/Cart.java
+++ src/main/java/com/example/datnsd56/entity/Cart.java
@@ -1,93 +1,43 @@
 package com.example.datnsd56.entity;
 
-import jakarta.persistence.Basic;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Builder
 @Entity
+@Getter
+@Setter
+@Table(name = "cart")
 public class Cart {
-    private int id;
-    private String status;
-    private Timestamp createDate;
-    private Timestamp updateDate;
-    private Account accountByAccountId;
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Id")
-    public int getId() {
-        return id;
-    }
+    private Integer id;
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
-    @Basic
+
     @Column(name = "status")
-    public String getStatus() {
-        return status;
-    }
+    private String status;
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
 
-    @Basic
+
     @Column(name = "create_date")
-    public Timestamp getCreateDate() {
-        return createDate;
-    }
+    private Date createDate;
 
-    public void setCreateDate(Timestamp createDate) {
-        this.createDate = createDate;
-    }
 
-    @Basic
     @Column(name = "update_date")
-    public Timestamp getUpdateDate() {
-        return updateDate;
-    }
-
-    public void setUpdateDate(Timestamp updateDate) {
-        this.updateDate = updateDate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cart cart = (Cart) o;
-        return id == cart.id && Objects.equals(status, cart.status) && Objects.equals(createDate, cart.createDate) && Objects.equals(updateDate, cart.updateDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, status, createDate, updateDate);
-    }
+    private Date updateDate;
 
     @ManyToOne
     @JoinColumn(name = "account_id", referencedColumnName = "id")
-    public Account getAccountByAccountId() {
-        return accountByAccountId;
-    }
+    private Account accountByAccountId;
 
-    public void setAccountByAccountId(Account accountByAccountId) {
-        this.accountByAccountId = accountByAccountId;
-    }
 }
