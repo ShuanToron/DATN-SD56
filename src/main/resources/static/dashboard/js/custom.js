5a3f84bee1e4abc990f5f6fd7a4e054f4d627415
ShuanToron <xtruongtdo2003@gmail.com>
2023-10-13 10:15:17 +0700

    Update form san pham
    Them nut them nhanh cho cac thuoc tinh ơ trang them sản pham

--- src/main/resources/static/dashboard/js/custom.js
+++ src/main/resources/static/dashboard/js/custom.js
@@ -2,9 +2,10 @@
  *
  * You can write your JS code here, DO NOT touch the default style file
  * because it will make it harder for you to update.
- * 
+ *
  */
-function handleFileSelect (evt) {
+// Hiện preview ảnh khi thêm sản phẩm
+function handleFileSelect(evt) {
     // Loop through the FileList and render image files as thumbnails.
     for (const file of evt.target.files) {
 
@@ -19,6 +20,43 @@ function handleFileSelect (evt) {
     }
 }
 
-document.getElementById('files').addEventListener('change', handleFileSelect, false);
+// Hiện form thêm nhanh loại giày
+function popupLoaiGiay() {
+    var popup = document.getElementById("formLoaiGiay");
+    if (popup.style.display === "block") {
+        popup.style.display = "none";
+    } else {
+        popup.style.display = "block";
+    }
+}
+
+// Hiện form thêm nhanh thương hiệu
+function popupThuongHieu() {
+    var popup = document.getElementById("formThuongHieu");
+    if (popup.style.display === "block") {
+        popup.style.display = "none";
+    } else {
+        popup.style.display = "block";
+    }
+}
+
+function popupChatLieu() {
+    var popup = document.getElementById("formChatLieu");
+    if (popup.style.display === "block") {
+        popup.style.display = "none";
+    } else {
+        popup.style.display = "block";
+    }
+}
+
+function popupDeGiay() {
+    var popup = document.getElementById("formDeGiay");
+    if (popup.style.display === "block") {
+        popup.style.display = "none";
+    } else {
+        popup.style.display = "block";
+    }
+}
+
 "use strict";
 
